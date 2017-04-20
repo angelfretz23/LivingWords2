26ebfc8c9a9bce5e67eccc9384c35b182cdb0dd3
Binariks <admin@binariks.com>
2017-04-20 20:00:03 +0300

    Added MainTableView Group(Folder), there created MainTableView.storyboard, model: Scripture, ViewController for managing tableView: MainTableViewController and TableViewCell class. Created main table view of the app, table view cell prototype in XIB file. Added test method loadSampleScriptures() and tested the work of table. Table represents data correctly.

---
+++ BibleApp/Scripture.swift
@@ -0,0 +1,9 @@
+//
+//  Scripture.swift
+//  BibleApp
+//
+//  Created by Ostap Romaniv on 4/20/17.
+//  Copyright © 2017 Igor Makara. All rights reserved.
+//
+
+import Foundation
---
+++ BibleApp/ScriptureTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScriptureTableViewCell.swift
+//  BibleApp
+//
+//  Created by Ostap Romaniv on 4/20/17.
+//  Copyright © 2017 Igor Makara. All rights reserved.
+//
+
+import UIKit
+
+class ScriptureTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
