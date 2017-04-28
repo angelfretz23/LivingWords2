b9c38031f9f1afd99d8fac8128969668641133ac
Oleh <opetriv@gmail.com>
2017-04-28 13:52:21 +0300

    added tab bar controller

--- BibleApp/HomeViewController.swift
+++ BibleApp/HomeViewController.swift
@@ -1,26 +1,28 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  BibleApp
 //
-//  Created by Igor Makara on 4/19/17.
+//  Created by ігор on 4/28/17.
 //  Copyright © 2017 Igor Makara. All rights reserved.
 //
 
 import UIKit
 
-
-class SplashViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.tabBarController?.tabBar.items![1].image = UIImage(named: "BookTabBarIcon")
+        self.tabBarController?.tabBar.items![1].title = "Read"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    
 
 }
-
