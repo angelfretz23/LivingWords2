c5035657f3c5fa0ff10f66bb55b1eb97a9ae9bcc
Oleh Petriv <opetriv@gmail.com>
2017-05-10 18:22:57 +0300

    Added Upload Music and Sermones UI

---
+++ BibleApp/BottomUploadMusicCell.swift
@@ -0,0 +1,24 @@
+//
+//  BottomUploadMusicCell.swift
+//  BibleApp
+//
+//  Created by Mac on 5/10/17.
+//  Copyright © 2017 Igor Makara. All rights reserved.
+//
+
+import UIKit
+
+class BottomUploadMusicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ BibleApp/MusicUploadTVC.swift
@@ -0,0 +1,24 @@
+//
+//  MusicUploadTVC.swift
+//  BibleApp
+//
+//  Created by Mac on 5/10/17.
+//  Copyright © 2017 Igor Makara. All rights reserved.
+//
+
+import UIKit
+
+class MusicUploadTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
