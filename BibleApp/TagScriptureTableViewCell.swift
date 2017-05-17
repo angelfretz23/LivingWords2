d77e9aba9391d9b93cda2aef167a76d96cd6c17f
Oleh <opetriv@gmail.com>
2017-05-17 18:04:55 +0300

    Make logic to select scriptures from different books and parse data between view controllers

--- BibleApp/TagScriptureTableViewCell.swift
+++ BibleApp/TagScriptureTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class TagScriptureTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var tagScriptureLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
