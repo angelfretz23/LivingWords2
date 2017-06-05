1c5676ecaeb59afcfd4be232f7c9e8433428b82f
Angel Contreras <angel.contreras23@me.com>
2017-06-04 18:22:59 -0700

    login screen complete with no functionality

---
+++ BibleApp/NewLogin/Controllers/NewLoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewLoginViewController.swift
+//  BibleApp
+//
+//  Created by Angel Contreras on 6/4/17.
+//  Copyright © 2017 Igor Makara. All rights reserved.
+//
+
+import UIKit
+
+final class NewLoginViewController: UIViewController{
+    
+    @IBOutlet weak var backgroundBlur: UIView!
+    
+    override func viewDidLoad() {
+    }
+    
+    internal override var prefersStatusBarHidden: Bool{
+        return true
+    }
+}
+
