c3aec7cc90b9e1657a63824508f8e98d20171f48
Angel Contreras <angel.contreras23@me.com>
2017-06-09 16:17:10 -0700

    added email verifier

---
+++ BibleApp/NewLogin/Controllers/EmailVerifierController.swift
@@ -0,0 +1,18 @@
+//
+//  EmailVerifierController.swift
+//  BibleApp
+//
+//  Created by Angel Contreras on 6/9/17.
+//  Copyright © 2017 Igor Makara. All rights reserved.
+//
+
+import Foundation
+
+class EmailVerifier{
+    public static func isValid(email: String, completion: ((_ valid: Bool) -> Void)) {
+        let filterString = "[A-Z0-9a-z\\._%-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", argumentArray: [filterString])
+        
+        completion(emailTest.evaluate(with: email, substitutionVariables: nil))
+    }
+}
